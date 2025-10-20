9029635e17caf35ef14ff9649625f00adcf64616
stefan <stefan.holmberg@systementor.se>
2025-10-20 11:32:44 +0200

    memory

--- main.c
+++ main.c
@@ -42,13 +42,49 @@ void createNew(Player *newPlayer){
     // Felhantering - loopa till alla är "ok"
     printf("Skapa ny\n************************\n");
     INPUT_RESULT result = GetInput("Name:",newPlayer->name,20);
-    if(result == INPUT_RESULT_OK ){
+    if(result == INPUT_RESULT_TOO_LONG){
         printf("Du får bara skriva in 20 tecken");
     }
     
     bool successfulInput = GetInputInt("Jersey:",&newPlayer->jersey);
     // printf("Jersey:");
     // scanf(" %d",&newPlayer->jersey); // om error = ingen clear
+}
+
+typedef struct{
+    Player *allPlayers;
+    int playersCount;
+} PLAYERLIST;
+
+
+void adminMenu(){
+    int bla = 12;
+    while(){
+    // skriv ut admin
+    // 1, ta fram rapport
+    // 3. exit
+    int sel;
+    scanf(" %d",&sel);
+    if(sel == 3){
+       return;
+    }
+    }
+}
+
+
+void huvudMenu(){
+    int age = 12;
+    while(){
+    // skriv ut huvudmenyn
+    // 1, Admin
+    // 2, Skapa
+    // 3. exit
+    int sel;
+    scanf(" %d",&sel);
+    if(sel == 1){
+        adminMenu();
+    }
+    }
 }
 
 
@@ -62,15 +98,40 @@ int main(){
     // Den ska kunna växa!
 
     int age;
-//    Player allPlayers[10]; // ALLOKERAD PÅ STACKEN - 10 * sizeof(Player)
+    // 
+    //Player allPlayers[10]; // ALLOKERAD PÅ STACKEN - 10 * sizeof(Player)
+
+    // Python = listan är så stor som vi har saker i den
+    // C array har två "sizes" - MINNESALLOKERING - använder
+    // 0
+
 //    allPlayers[111] = 
     //Player allPlayers[]; 
-    Player *allPlayers; // pekare till en eller flera Player - odefinierat värde
-    allPlayers = malloc( 10 * sizeof(Player) ); // int  ALLTID bytes!!!
+    PLAYERLIST playersList;
+    playersList.allPlayers = NULL;
+    playersList.playersCount = 0;
+    // Player *allPlayers = NULL; // pekare till en eller flera Player - odefinierat värde
+    // int playersCount = 0;
+    //allPlayers = malloc( 10 * sizeof(Player) ); // int  ALLTID bytes!!!
+
     // allPlayers arrayen ligger på HEAPEN inte på stacken
     // Stefan förklarar heap efter rasten
     // Men den STORA skillnaden är att nu kan allPlayers VÄXA - men fortsätta vara en array (konsekutivt i minnet)
     // heap kommer at a price
+
+    // en stackallokerad variabel lever tills SCOPE:t är slut
+    // en heapallokerad variabel lever tills vi frigör den
+
+    if(1){
+        char *s = malloc(100);
+        free(s);
+        // s är en pekare 
+        // s ligger på stacken
+        // men det den pekar på ligger på heapen
+    }
+    // s försvinner
+    // men det som s pekade på ligger på heapen
+    
 
 
     int b;
@@ -80,15 +141,39 @@ int main(){
         GetInputInt("Ange val:",&selection);
         switch(selection){
             case 1:
-                Player player;
-                createNew(&player);
-                printf("Ny spelare som heter:%s\n", player.name);
+                // Förstora array med 1 
+                // MALLOC första gången!!!
+                // andra gången och senare köra man REALLOC
+
+                // allPlayers = realloc(allPlayers,(playersCount  + 1) * sizeof(Player));
+
+                if(playersList.playersCount == 0){
+                    playersList.allPlayers = malloc(sizeof(Player));  // void * betyder pekare till vad som helst
+                } else {
+                    // "storleken" just nu PLUS 1 * sizeof(Player)
+                    // (playersCount  + 1) * sizeof(Player)
+                    // playersCount * sizeof(Player)
+                    playersList.allPlayers = realloc(playersList.allPlayers,(playersList.playersCount  + 1) * sizeof(Player));
+                }
+
+                createNew(&playersList.allPlayers[playersList.playersCount]);
+
+                playersList.playersCount++;
+                
+//                Player player;
+//                createNew(&player);
+                //printf("Ny spelare som heter:%s\n", player.name);
                 //Put player into a "list"  (dvs array!) (konsektivt minne)
                 break;
             case 2:
-                printf("Lista alla");
+                printf("Lista alla\n");
+                printf("***********************\n");
+                for(int i = 0; i < playersList.playersCount;i++){
+                    printf("Spelare:%s\n", playersList.allPlayers[i].name);
+                }
                 break;
             case 3:
+                free(playersList.allPlayers); // JAG VISAR ATT JAG VET ATT JAG SKA LÄMNA TILLBAKA
                 return 0;
         }
     }
