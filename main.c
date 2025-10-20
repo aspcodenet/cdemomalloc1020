82f53bff86176d01f89d1e861002ef8da5a89bff
stefan <stefan.holmberg@systementor.se>
2025-10-20 08:45:40 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct {
+    int jersey;
+    char name[20];
+}Player;
+
+
+int main(){
+
+    while(1){
+        printMenu();
+        
+    }
+
+    return 0;
+}
+
+
