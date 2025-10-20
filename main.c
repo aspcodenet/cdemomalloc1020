e71b386b5d07e88fe074c0abd0c26ec6e876dbb3
stefan <stefan.holmberg@systementor.se>
2025-10-20 08:48:43 +0200

    a

--- main.c
+++ main.c
@@ -5,12 +5,30 @@ typedef struct {
     char name[20];
 }Player;
 
+void printMenu(){
+    printf("1. Skapa ny");
+    printf("2. Lista alla");
+    printf("3. Exit");
+}
+
 
 int main(){
 
     while(1){
         printMenu();
-        
+        int selection;
+        scanf(" %d", &selection);
+        switch(selection){
+            case 1:
+                printf("Ny");
+                break;
+            case 2:
+                printf("Lista alla");
+                break;
+            case 3:
+                return;
+        }
+
     }
 
     return 0;
